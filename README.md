45bd4e743222bd8a0e617b500bb9c834e5c7ae04
mrvol <mr.volodkovi4@gmail.com>
2025-05-01 16:47:39 +0300

    scan api mono

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javarush
+
+coming soon
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("first commit")
+"""def openOrSenior(data):
+  return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]"""
+def open_or_senior(data):
+    my_list = []
+    for i in data:
+        print(f'{i[0]}: {i[1]}')
+        if i[0] >= 55 and i[1] > 7 :
+            my_list.append('Senior')
+        else:
+            my_list.append('Open')
+    return my_list
+input =  [(14, 10),  (54, 17), (70, 24), (24, 26),(54, 8), (48, 16)]
+data = open_or_senior(input)
+print(data)
+
+
